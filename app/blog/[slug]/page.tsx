01cef5bf7d9ff597be1128259d3625bef52b5596
AnhThien13 <thienbmt1310@gmail.com>
2024-10-15 14:59:07 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -10,7 +10,7 @@ export async function generateMetadata({ params }: Params) {
 
 export default function Page({ params }: Params) {
   return <><h1>Slug: {params.slug}</h1>
-   <p>TestTA</p>
+   <p>Test TA</p>
    <p>Hi bae</p>
     </>;
 }
